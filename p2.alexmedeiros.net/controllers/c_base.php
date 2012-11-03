b5acc4acf13500eb57144ca88c9d4d55ced40ebc
Alex Medeiros <ale.med27@gmail.com>
2012-11-03 13:53:44 -0400

    Framework update (10/23) and Part 1 App progress

--- p2.alexmedeiros.net/controllers/c_base.php
+++ p2.alexmedeiros.net/controllers/c_base.php
@@ -16,15 +16,15 @@ class base_controller {
 			$this->userObj = new User();
 			
 		# Authenticate / load user
-			$this->user = $this->userObj->authenticate();			
-							
-		# Set up templates
+			$this->user = $this->userObj->authenticate();
+			
+		# Set up us the templates
 			$this->template 	  = View::instance('_v_template');
-			$this->email_template = View::instance('_v_email');			
-								
-		# So we can use $user in views			
+			$this->email_template = View::instance('_v_email');	
+			
+		# So we can use $user in views
 			$this->template->set_global('user', $this->user);
 			
 	}
 	
-} # eoc
+}
