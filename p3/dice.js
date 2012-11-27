2fc7149b04c3031a3987b7e0648730e2965a1a08
Alex Medeiros <ale.med27@gmail.com>
2012-11-26 20:54:57 -0500

    P3 commit and minor css changes from P2 feedback

---
+++ p3/dice.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+	$('#red').click(function() {$("#dice_roll").css("background-image", "url('red.png')");});
+	$('#green').click(function() {$("#dice_roll").css("background-image", "url('green.png')");});
+	$('#blue').click(function() {$("#dice_roll").css("background-image", "url('blue.png')");});
+	
+	$("input:button[name=roll]").click(function dice_roll() {
+
+		/* get number of dice from radio button selection */
+		var dice_num = $("input:radio[name=number]:checked").val();
+		
+		/* debug to determine if dice count is correct on roll */
+		//console.log(dice_num);
+
+		/* clear results for reroll */
+		$("#dice").html("");
+
+		var roll = new Array();
+		/* for each roll generate a (pseudo)random number between 1 and 6 */
+		for (i = 0; i < dice_num; i += 1) {
+			roll[i] = Math.floor(Math.random() * 6) % 6 + 1;
+		}
+
+		/* debug to view roll array */
+		//console.log(roll);
+			
+		/* add total of rolls */
+		var total = 0;
+		for (i = 0; i < roll.length; i += 1) {
+			total += parseInt(roll[i]);
+		}
+		
+		/* debug to view total */
+		//console.log(total);
+		
+		/* concatenate into div and display image per die class */
+		for (i = 0; i < roll.length; i += 1) {
+			$("#dice").append("<span class='die" + roll[i] + "'>" + roll[i] + "</span>");
+			$(".die" + roll[i]).html("<img src='" + roll[i] + ".png' alt='" + roll[i] + "'>");
+		}
+		
+		/* display total */
+		$("#total_text span").html("<div id='total_num'>" + total + "</div>");
+
+	});
+	
+});
