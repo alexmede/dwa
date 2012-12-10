6de69a904240e8a6a3a22dd324b74d40cfff3921
Alex Medeiros <ale.med27@gmail.com>
2012-12-10 17:02:51 -0500

    setup p4 subdomain

---
+++ p4.alexmedeiros.net/index.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>DWA Projects</title>
+	
+	<meta name="description" content="DWA Projects" />
+	
+	<link rel="icon" type="image/png" href="/dwa/favicon.png" />
+
+</head>
+
+<body>
+
+	<h1 id="home"><a href="http://alexmedeiros.net/" title="Home">alexmedeiros.net</a></h1>
+	
+			<div class="content">
+				<p>Placeholder</p>
+			</div>
+	
+</body>
+
+</html>
